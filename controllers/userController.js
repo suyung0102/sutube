a76f10558e203cd3a87a2d0a9bb9f395bf740fa4
suyung0102 <suyung0102@gmail.com>
2020-07-03 18:23:12 +0900

    node.js 연동 및 미들웨어 설치

---
+++ controllers/userController.js
@@ -0,0 +1,3 @@
+export const login =(req, res) => res.send("Login");
+export const logout =(req, res) => res.send("Logout");
+export const search =(req, res) => res.send("Search");
---
+++ controllers/videoController.js
@@ -0,0 +1,2 @@
+export const home =(req, res) => res.send("Home");
+export const join =(req, res) => res.send("Join");
